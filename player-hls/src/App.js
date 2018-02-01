54e88306230abfd445515d6cbf4e48de36d00016
Vivian <chrysophyta@gmail.com>
2018-01-31 21:56:41 -0500

    remove un-used imports in App.js

--- player-hls/src/App.js
+++ player-hls/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Player from './Player';
 import Controls from './Controls';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
